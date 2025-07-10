5d12d5ad340e96ea49bb0c83cc648fd7c9f4081d
h662 <h662hong@gmail.com>
2025-07-10 11:07:18 +0900

    Craete Comment

--- src/main/java/com/h662/simple_board/controller/LoginController.java
+++ src/main/java/com/h662/simple_board/controller/LoginController.java
@@ -1,11 +1,17 @@
 package com.h662.simple_board.controller;
 
 import com.h662.simple_board.dto.LoginDto;
+import com.h662.simple_board.model.User;
 import com.h662.simple_board.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +23,34 @@ public class LoginController {
         model.addAttribute("loginDto", new LoginDto());
 
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @Valid @ModelAttribute LoginDto loginDto,
+            BindingResult bindingResult,
+            HttpSession httpSession,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) return "login";
+
+        User user = userRepository.findByUsername(loginDto.getUsername()).orElse(null);
+
+        if (user == null || !user.getPassword().equals(loginDto.getPassword())) {
+            model.addAttribute("error", "아이디/비밀번호가 올바르지 않습니다");
+
+            return "login";
+        }
+
+        httpSession.setAttribute("user", user);
+
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+
+        return "redirect:/login";
     }
 }
